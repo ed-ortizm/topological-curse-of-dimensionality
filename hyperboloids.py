f6fba7d8209e587b6217f87ffdbe38fe4a74ad97
Edgar Ortiz <ed.ortizm@gmail.com>
2020-11-04 16:43:08 -0300

    Experiments on hyperbolic spaces working

--- hyperboloids.py
+++ hyperboloids.py
@@ -4,48 +4,60 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-def dist(x, y=None, p=1):
-    ## https://en.wikipedia.org/wiki/Hyperbolic_space#:~:text=Hyperbolic%20space%20is%20a%20space,also%20called%20the%20hyperbolic%20plane.
-    if y==None:
-        y = np.zeros(x.shape)
-        y[:,-1] = 1
+from lib_metrics_spaces import hyp_dist
 
-    B_xy = (x[:,-1]*y[:,-1])**p - np.sum((x[:, :-1]*y[:, :-1])**p, axis=1)
-    B_xy **= 1/p
+## Wikipedia page for Hyperbolic space
 
-    return np.arccosh(B_xy)
+# Euclid's parallel postulate is no longer assumed to hold. Instead, the parallel
+# postulate is replaced by the following alternative (in two dimensions):
+#
+#  *Given any line L and point P not on L, there are at least two distinct lines
+#   passing through P which do not intersect L.
+#
+# There are several important models of hyperbolic space: the Klein model, the
+# hyperboloid model, the Poincaré ball model and the Poincaré half space model.
+# These all model the same geometry in the sense that any two of them can be
+# related by a transformation that preserves all the geometrical properties of
+# the space, including isometry (though not with respect to the metric of a
+# Euclidean embedding).
+
+## Here I implement the Hyperboloid model, where the n-hyperbolic space is
+# embedded in R^{n+1} --> x_o^2 - x_1^2 - ... - x_n^2 = 1, x_o > 0
+
+# In this model a line (or geodesic) is the curve formed by the intersection
+# of H^n with a plane through the origin in R^{n+1}.
 
 np.random.seed(0)
+ploth_path = './plots/hyperbolic_spaces'
 
-
+H_scale = 10_000_000
 N = 1_000 # number of points to generate in the n-sphere
-nn = np.arange(1,101)
-pp = np.array([1/3, 1/2, 2/3, 1, 2, 3, 5, 10])
-D_mm = np.empty(100)
+nn = np.arange(1,201)
+# pp = np.array([1/3, 1/2, 2/3, 1, 2, 3, 5, 10])
+D_mm = np.empty(nn.size)
 
-for p in pp:
+for n in nn:
 
-    for n in nn:
+    Hn = H_scale*(np.random.random(size=(N, n+1)) - 0.5) # embedded in Euclidean space
+    Hn[:, 0] = np.sqrt(1 + np.sum(Hn[:, 1:]**2, axis=1))
+    Y = Hn[:, np.random.randint(low=0, high=n+1)]
+    d = hyp_dist(Hn, Y)
+    if n==1:
+        print(Y[0:10])
+    if n==100:
+        print(Y[0:10])
 
-        x = np.empty((N, n+1 )) # embedded in Euclidean space
+    D_mm[n-1] = np.max(d) - np.min(d)
 
-        for idx in range(N):
-            # x[idx, 0:n] = np.random.normal(loc=0, scale=1, size=n)
-            x[idx, 0:n] = np.random.random(size=n) - 0.5
-            # both work, notheless I keep the first one, after I check the paper, I'll see.
+fig, ax = plt.subplots(figsize=(10,5))
 
-        x[:, n] = np.sqrt(1 + np.sum(x[:, :n]**2, axis=1))
+ax.scatter(nn[2:], D_mm[2:])
+plt.tight_layout()
 
-        d = dist(x, p=p)
-        D_mm[n-1] = np.max(d) - np.min(d)
+fig.savefig(f'{ploth_path}/hyperbic_distance.png')
+fig.savefig(f'{ploth_path}/hyperbic_distance.pdf')
 
-    fig, ax = plt.subplots(figsize=(10,10))
-
-    ax.scatter(nn, D_mm)
-    # ax.set_aspect('equal', adjustable='box')
-    # plt.show()
-    # plt.close()
-    fig.savefig(f'./hyperbolic/hyperbola_distance_dimensions_p_{p:.2f}.png')
+plt.close()
 
 
 ## 1-hyperbola
--- lib_metrics_spaces.py
+++ lib_metrics_spaces.py
@@ -20,6 +20,25 @@ def arccos_metric(X, Y=None):
 
     return np.arccos(np.sum(X*Y, axis=1))
 ################################################################################
+def Q(X):
+    return X[:, 0]**2 - np.sum(X[:, 1:]**2, axis=1)
+
+def hyp_dist(X, Y, origin='default'):
+    ## https://en.wikipedia.org/wiki/Hyperbolic_space#:~:text=Hyperbolic%20space
+    ## %20is%20a%20space,also%20called%20the%20hyperbolic%20plane.
+
+    if origin=='default':
+        # Y = np.zeros(X.shape)
+        # Y[:,0] = 1. # reference point
+        X[:, 0] += 1
+        B_xy = 0.5*( Q(X) - 2)
+
+        return np.arccosh(B_xy)
+
+    B_xy = 0.5*( Q(X+Y) -2 )
+
+    return np.arccosh(B_xy)
+################################################################################
 
 def plot(x, y, fname, path, figsize=(10,5)):
 
