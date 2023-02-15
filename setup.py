8abff3b6568162eb1d05130c0a3b5266bbd8f56d
Edgar Ortiz Lenovo <ed.ortizm@gmail.com>
2023-02-15 17:04:24 -0500

    chore: add setupt file to release branch

---
+++ setup.py
@@ -0,0 +1,28 @@
+"""Setup script for topocurse package."""
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setup(
+    name="topocurse",
+    version="1.0.0",
+    install_requires=[
+        "matplotlib == 3.5.1",
+        "numpy == 1.22.3"
+    ],
+    author="Edgar Ortiz",
+    author_email="ed.ortizm@gmail.com",
+    packages=find_packages(where="src", include=["[a-z]*"], exclude=[]),
+    package_dir={"": "src"},
+    description="Explore the curse of dimensionality in different manifolds",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ed-ortizm/topological-curse-of-dimensionality",
+    license="MIT",
+    keywords=(
+        "curse of dimensionality, euclidean space, hyperbolic space,"
+        "Sphere, manifold,topology"
+    ),
+)
