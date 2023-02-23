9c5e76ea67a503cd130cecf363b6c04367111f1d
Edgar Ortiz Lenovo <ed.ortizm@gmail.com>
2023-02-23 11:44:33 -0500

    chore: conform to flake8

--- euclidean/euclidean_space.py
+++ euclidean/euclidean_space.py
@@ -3,54 +3,52 @@
 import os
 import time
 
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
-from lib_metrics_spaces import lp, plot
 from topocurse.sampling import random_points_euclidean
 from topocurse.metrics import lp_distance
+from topocurse.figures import contrast_plot
 
-
-ti = time.time()
+start_time = time.perf_counter()
 
 path_plot = "/home/edgar/Downloads/topo/euclidean"
 
 if os.path.exists(path_plot) is False:
     os.mkdir(path_plot)
 
+# number of points to sample
+N = 1_000
+# dimensionality
+nn = np.arange(1, 201)
+# metric's parameter
+pp = [i / 10.0 for i in range(1, 11)] + [2, 3, 5, 10, 20]
+# array to store distances
+D_mm = np.empty(nn.size)
 
-
-N = 1_000  # number of points to sample
-nn = np.arange(1, 201)  # dimensionality
-pp = [i / 10.0 for i in range(1, 11)] + [2, 3, 5, 10, 20]  # metric's parameter
-D_mm = np.empty(nn.size)  # array to store distances
-
-## Sampling points
-
+# Sampling points
 np.random.seed(0)
 X = random_points_euclidean(n=nn.size, N=N)
-# X = np.random.rand(N, nn.size)
 
 for p in pp:
 
     for n in nn:
 
         d = lp_distance(X=X[:, : n + 1], p=p)
-        # (X=X[:, : n + 1], p=p)
-        # d = lp(X=X[:, : n + 1], p=p)
         D_mm[n - 1] = np.max(d) - np.min(d)
 
-    plot(
-        x=nn,
-        y=D_mm,
-        fname=f"contrast_euclidean_p_{p}",
-        path=path_plot,
-        title="Distance behavior in the n-d Euclidean Space",
+    fig, ax = contrast_plot(
         metric="d(x,y)=L_p(x,y)",
+        title="Distance behavior in the n-d Euclidean Space",
         euclidean=True,
         p=p,
     )
 
-tf = time.time()
+    ax.scatter(nn, D_mm)
+    fig.savefig(f"{path_plot}/contrast_euclidean_p_{p}.png")
+    fig.savefig(f"{path_plot}/contrast_euclidean_p_{p}.pdf")
+    plt.close()
 
-print(f"Running time: {tf-ti:.2f} seconds")
+finish_time = time.time()
+
+print(f"Running time: {finish_time - start_time:.2f} seconds")
